e484f261fea11c6b6abdd00b8627e2808b86451f
Rob DelValle <robdel13@gmail.com>
2016-12-06 01:32:52 -0700

    more gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-var gulp = require('gulp'),
-	sourcemaps = require('gulp-sourcemaps'),
+var gulp = require('gulp');
+	// sass = require('gulp-sass')
+var	sourcemaps = require('gulp-sourcemaps');
 	// del = require('del'),
-	babel = require('gulp-babel'),
-	uglify = require('gulp-uglify'),
-	print = require('gulp-print'),
-	concat = require('gulp-concat');
+	// babel = require('gulp-babel'),
+	// uglify = require('gulp-uglify'),
+	// print = require('gulp-print'),
+	// concat = require('gulp-concat');
 
 var CacheBuster = require('gulp-cachebust');
 var cachebust = new CacheBuster();
-var deploy = require('gulp-gh-pages');
+var deploy      = require('gulp-gh-pages');
 
 gulp.task('default', function() {
 	console.log("hello");
