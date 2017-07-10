e5ad79d8c41a90d63a2622ea3b8508c8e066dd5b
Rob <robdel13@gmail.com>
2017-07-10 01:58:28 -0700

    changed deploy method on heroku to use node

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-/**
- * Created by robdelvalle on 7/10/17.
- */
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Running on port', app.get('port'));
+});
